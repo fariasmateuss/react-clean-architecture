bafee123b85dedcb46791cca39248838cd2589ae
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-04-07 14:46:25 -0400

    refactor: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/usercases/authentication/remote.authentication.ts
+++ src/data/usercases/authentication/remote.authentication.ts
@@ -1,3 +1,4 @@
+import { AuthenticationParams } from '../../../domain/usercases/authentication'
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteAuthentication {
@@ -6,8 +7,8 @@ export class RemoteAuthentication {
     private readonly httpPostClient: HttpPostClient
   ) {}
 
-  async auth(): Promise<void> {
-    await this.httpPostClient.post({ url: this.url })
+  async auth(params: AuthenticationParams): Promise<void> {
+    await this.httpPostClient.post({ url: this.url, body: params })
     return Promise.resolve()
   }
 }
